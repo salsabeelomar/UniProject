f3fa3794ff8d14d98bdd85038a5300e3a4dc129e
salsabeelomar <omarsalsabeel65@gmail.com>
2023-05-08 21:51:19 +0300

    create the main structure for the controllers as classes relates #5

---
+++ controllers/Favorite.js
@@ -0,0 +1,8 @@
+class Favorite {
+  async addToFavorite(data) {}
+  async updateFavoriteItems(data) {}
+  async removeFavoriteItems(data) {}
+}
+
+const favorite = new Favorite();
+module.export = favorite;
---
+++ controllers/Product.js
@@ -0,0 +1,8 @@
+class Product {
+  async addProduct(data) {}
+  async updateProduct(data) {}
+  async getProducts(data) {}
+}
+
+const product = new Product();
+module.export = Product;
